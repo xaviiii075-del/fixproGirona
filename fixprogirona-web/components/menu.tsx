690f2eba160651adbecea94b951f1c19c50c77eb
Xavi <xaviiii075@gmail.com>
2026-06-15 22:24:59 +0200

    actualizar menu y contenidos

--- fixprogirona-web/components/menu.tsx
+++ fixprogirona-web/components/menu.tsx
@@ -4,12 +4,14 @@ export default function Menu() {
       <nav className="mx-auto flex max-w-6xl items-center justify-center gap-4 px-4 py-4 text-sm font-semibold overflow-x-auto">
 
         <a href="/">Inici</a>
-<a href="/aires-condicionats">
-  Aires Condicionats
-</a>
-        <a href="/electricitat">
+<a href="/electricitat">
           Electricitat
         </a>
+        <a href="/aires-condicionats">
+
+  Aires Condicionats
+
+</a>
 
         <a href="/fontaneria">
           Fontaneria
