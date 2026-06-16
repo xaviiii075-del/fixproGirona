4b004dc5282e5d4ea7ad4dd3f17ec3e59b4b0e5f
Xavi <xaviiii075@gmail.com>
2026-06-16 11:34:25 +0200

    menu hamburguesa funcional

--- fixprogirona-web/components/menu.tsx
+++ fixprogirona-web/components/menu.tsx
@@ -3,39 +3,20 @@ export default function Menu() {
     <header className="sticky top-0 z-50 bg-white border-b shadow-sm">
       <nav className="mx-auto flex max-w-6xl items-center justify-center gap-4 px-4 py-4 text-sm font-semibold overflow-x-auto">
 
-        <a href="/">Inici</a>
-<a href="/electricitat">
-          Electricitat
-        </a>
+        
+
+        
         <a href="/aires-condicionats">
 
   Aires Condicionats
 
 </a>
 
-        <a href="/fontaneria">
-          Fontaneria
-        </a>
+        
 
-        <a href="/reparacions-urgents">
-          Urgències
-        </a>
+        
 
-        <a href="/gas-i-installacions">
-          Calderes
-        </a>
-
-        <a href="/reformes-de-bany">
-          Banys
-        </a>
-
-        <a href="/manteniment">
-          Manteniment
-        </a>
-
-        <a href="/#contacte">
-          Contacte
-        </a>
+        
 
       </nav>
     </header>
